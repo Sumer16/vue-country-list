004e69a80b25538dc8d2e0801b8d00c9aa62ce7f
Sumer Patel <sumerdpatel@gmail.com>
2021-05-12 11:13:45 +0530

    modified Home & About view pages

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="bg-white px-8 py-4 md:py-20 md:px-60 m-auto dark:bg-gray-900">
+    <h1 class="text-4xl font-medium text-gray-800 mt-4 mb-2 dark:text-gray-500">About</h1>
+    <div class="text-gray-800 py-1 sm:text-center md:text-left dark:text-gray-300">
+      <p class="pb-3">This web application is intended as a simple, easy to use way of providing the required information in a more user-friendly way about the countries & regions of the world.</p>
+      <p>All data is courtesy of REST Country API project on Github.</p>
+    </div>
+    <h1 class="text-4xl font-medium text-gray-800 mt-4 mb-2 dark:text-gray-500">Contact</h1>
+    <div class="text-gray-800 py-1 sm:text-center md:text-left dark:text-gray-300">
+      <p>If you have any questions or suggestions regarding the countries list application, please feel free to drop me a line at sumer.patel@outlook.com</p>
+      <p class="flex flex-wrap sm:justify-end justify-center font-mono pt-5 mb-10">Copyright @ 2021 Sumer Patel</p>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+/* .about{
+  height: calc(100% - 129px);
+} */
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-white h-full overflow-y-auto py-5 px-1 my-8 mx-1 grid grid-col md:grid-cols-3 sm:grid-cols-1 gap-6 justify-items-center dark:bg-gray-900">
+    <!-- <h1 class="text-gray-900 dark:text-gray-100">Dark mode is here!</h1>
+    <p class="text-gray-600 dark:text-gray-400">Welcome to Country List</p> -->
+    <div v-for="arr in allCountries" :key="arr.name">
+      <h5 class="text-gray-900 dark:text-gray-100">{{ arr.name }}</h5>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      allCountries: []
+    }
+  },
+  created() {
+    axios.get(`https://restcountries.eu/rest/v2/all`)
+    .then(res => {
+      const countries = res.data;
+      for (const key of countries) {
+        this.allCountries.push(key);
+        console.log(key);
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }
 }
 </script>
