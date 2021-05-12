280417e4925ba810afc25fd48e525d446e49f8ed
Sumer Patel <sumerdpatel@gmail.com>
2021-05-12 11:14:15 +0530

    added Search view

---
+++ src/views/Search.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="h-full my-1 mx-1 overflow-y-auto">
+    <h1 class="px-4 py-3 uppercase text-4xl text-center font-mono dark:text-gray-500">Search By</h1>
+    <div class="flex flex-col justify-center items-center my-1">
+      <div v-for="d in search" :key="d.title" class="w-3/4 sm:w-3/5 my-4 mx-4 shadow-lg rounded-md py-3 px-1 bg-gray-200 flex flex-col items-center dark:bg-gray-800">
+        <p class="uppercase text-2xl font-mono dark:text-gray-500">{{ d.title }}</p>
+        <span class="text-base font-light dark:text-gray-300">{{ d.subtitle}}</span>
+        <router-link :to="d.route" class="text-base text-gray-700 hover:text-teal-500 focus:text-teal-500 transition-all duration-200">Search Here!</router-link>
+        <!-- <router-view></router-view> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: 'Search',
+  data() {
+    return {
+      search: [
+        {
+          title: 'Country',
+          subtitle: 'E.g. Denmark, Aruba, etc.',
+          route: '/search-by/country'
+        },
+        {
+          title: 'Capital',
+          subtitle: 'E.g. Paris, London, etc.',
+          route: '/search-by/capital'
+        },
+        {
+          title: 'Region',
+          subtitle: 'E.g. Europe, Africa, etc.',
+          route: '/search-by/region'
+        },
+        {
+          title: 'Calling Code',
+          subtitle: 'E.g. 372, 91, etc.',
+          route: '/search-by/calling-code'
+        },
+        {
+          title: 'Currency Code',
+          subtitle: 'E.g. inr, cop, etc.',
+          route: '/search-by/currency-code'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* .search{
+  height: calc(100% - 129px);
+} */
+</style>
